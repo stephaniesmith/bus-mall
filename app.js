a5cd5c26a7b9b42bea40320ccc54462075c62ce6
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-12 14:19:37 -0800

    created new Products

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ function Product (name, filePath, timesShown, timesClicked) {
     this.timesShown = 0;
     this.timesClicked = 0;
 };
+
+new Product ('bag', 'img/bag.jpg');
+new Product ('banana', 'img/banana.jpg');
+new Product ('bathroom', 'img/bathroom');
+new Product ('boots', 'img/boots.jpg');
+new Product ('brefast', 'img/breakfast.jpg');
+new Product ('bubblegum', 'img/bubblegum.jpg');
+new Product ('chair', 'img/chair.jpg');
+new Product ('cthulhu', 'img/cthulhu.jpg');
+new Product ('dog-duck', 'img/dog-duck.jpg');
+new Product ('dragon', 'img/dragon.jpg');
+new Product ('pen', 'img/pen.jpg');
+new Product ('pet-sweep', 'img/pet-sweep.jpg');
+new Product ('scissors', 'img/sissors.jpg');
+new Product ('shark', 'img/shark.jpg');
+new Product ('sweep', 'img/sweep.png');
+new Product ('tauntaun', 'img/tauntaun.jpg');
+new Product ('unicorn', 'img/unicorn.jpg');
+new Product ('usb', 'img/usb.gif');
+new Product ('water-can', 'img/water-can.jpg');
+new Product ('wine-glass', 'img/wine.glass.jpg');
