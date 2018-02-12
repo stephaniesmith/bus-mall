b1132935d26343caa685e28b4f08a074cd5df00a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-12 13:56:55 -0800

    added images

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function Product (name, filePath, timesShown, timesClicked) {
+    this.name = name;
+    this.filePath = filePath;
+    this.timesShown = 0;
+    this.timesClicked = 0;
+};
